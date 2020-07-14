f2ef01b8bb814670bc4e06e6f5d049a773c626ba
SergiuszN <serg.ni2011@gmail.com>
2020-07-14 20:50:03 +0200

    Base admin page

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_home")
+     */
+    public function home()
+    {
+        return $this->render('admin/home.html.twig');
+    }
+}
