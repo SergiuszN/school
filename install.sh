58e716ec92583c9b193044212bb99d244cc3048f
sergiusz_ni <sergiusz.nikolenko@coffeedesk.pl>
2024-02-04 19:01:25 +0100

    Bump to 7.0 SF and 8.3 php

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ git pull
 composer install --no-dev --optimize-autoloader
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 php bin/console doctrine:schema:update --dump-sql --force
-# php bin/console ckeditor:install
+php bin/console ckeditor:install --tag=4.22.1 --clear=drop
+php bin/console assets:install public
